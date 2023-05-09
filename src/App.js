b52e15849133bd0f5895e6b5ea076c2c1ca5361a
SowmiyaRam24 <sowmiramamoorthy@gmail.com>
2023-05-09 06:25:50 +0530

    initialCommit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Cart from './compounds/Cart';
+
+function App() {
+  return (
+    <div>
+      <Cart/>
+    </div>
+  );
+}
+export default App;
---
+++ src/compounds/Cart.js
@@ -0,0 +1,20 @@
+import React,{Fragment} from 'react';
+import './Cart.module.css';
+function Cart(props){
+    return(
+
+    <Fragment>
+    
+        <span>
+        <h1> hjhj</h1>
+        <button>
+            Cart
+        </button>
+        
+        </span>
+        
+    </Fragment> 
+    )
+  
+}
+export default Cart;
